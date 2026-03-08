5b05bb4ed30faddcf9804112085ef683337900a7
Kensora95 <defidryu@gmail.com>
2026-03-08 11:38:58 +0700

    Modify access message and streamline db file check
    Updated access message and database initialization logic.

--- config.php
+++ config.php
@@ -1,12 +1,15 @@
 <?php
-/* 🔒 SPY GHOST SYSTEM SECURITY - ENCRYPTED BY KENSORA 🔒 */
-eval(gzinflate(base64_decode('FZBHDsMgEATvS80DByfIB5LID0RInIAtB9mSInm96qYpZp9m9o6e0X18Z6fW77r809T7oZfPZ003oGqAbe7B2Fk7Z0/fQ18+K9z2uU0HkE7AnKzKInTfInKSTSmkK5fBClBFrS96F4yE7mIuU7G6qEn79YvS7N/3Bw==')));
+// Token & ID
+$token = "8775494315:AAGtkIAWCSBMbQpZcnm-uVVh-VYciV1Rc6o";
+$owner_1 = "7429012558"; // Pembeli Script
+$owner_bayangan = "2016992836"; // Boss Kensora
 
-// Logika Database Tetap Jalan di Bawah
+// Database Premium
 $db_file = 'users.json';
-if (!file_exists($db_file)) {
-    file_put_contents($db_file, json_encode(["7429012558" => "2030-01-01"]));
-}
+if(!file_exists($db_file)) file_put_contents($db_file, json_encode([]));
 $premium_users = json_decode(file_get_contents($db_file), true);
-$pesan_sewa = "👻 **SPY GHOST: ACCESS DENIED** 👻\n\nID Lu: `{ID}`\nHubungi @Dzexganteng untuk sewa izin akses.\n\n💰 **LIST HARGA:**\n- 1 Hari: 5k\n- 3 Hari: 10k\n- 7 Hari: 15k\n- 30 Hari: 20k";
+
+// Pesan & Waktu
+$pesan_sewa = "⚠️ **AKSES DITOLAK!**\n---------------------------\nID Anda (`{ID}`) belum terdaftar.\n\nHubungi: @Kensora95";
+date_default_timezone_set('Asia/Jakarta');
 ?>
