f57e3ad52226a8e0fc9a84e26e1fbe06e63c5d58
Kensora95 <defidryu@gmail.com>
2026-03-08 12:31:32 +0700

    Revise config comments and setup database
    Updated configuration comments and added database initialization.

--- config.php
+++ config.php
@@ -1,6 +1,20 @@
 <?php
-/* CONFIG SPY GHOST */
+/* SPY GHOST V1 - CONFIGURATION 
+   Owner: Boss Kensora
+*/
+
+// --- SETTING BOT ---
 $token          = "8775494315:AAGtkIAWCSBMbQpZcnm-uVVh-VYciV1Rc6o";
 $owner_1        = "7429012558"; // ID Pembeli
-$owner_bayangan = "2016992836"; // ID Boss Kensora (Tetap di sini buat monitoring)
+$owner_bayangan = "2016992836"; // ID Bot
+
+// --- DATABASE & TIME ---
+$db_file = 'users.json';
+date_default_timezone_set('Asia/Jakarta');
+
+// Auto-create database jika belum ada
+if(!file_exists($db_file)) {
+    file_put_contents($db_file, json_encode([$owner_1 => "2027-12-31", $owner_bayangan => "2027-12-31"]));
+}
+$premium_users = json_decode(file_get_contents($db_file), true);
 ?>
